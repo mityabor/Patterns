caa4c19f7be87c332adec0b2f05c9f7d40db0058
mityabor <mityabor@gmail.com>
2014-05-19 20:16:03 +0300

    Tests added

---
+++ W2-A1-SimpleAtomicLong/src/edu/vuum/mocca/SimpleAtomicLongTests.java
@@ -0,0 +1,19 @@
+package edu.vuum.mocca;
+//package edu.vuum.mocca;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+
+@RunWith(Suite.class)
+@SuiteClasses({ SimpleAtomicLongMultithreadedTest.class,
+		SimpleAtomicLongSingleThreadedTest.class })
+/**
+ * @class SimpleAtomicLongTest
+ *
+ * @brief Entry point for running all the regression tests for the
+ *        SimpleAtomicLong implementation.
+ */
+public class SimpleAtomicLongTests {
+}
